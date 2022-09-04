dcfffb54acd6e69791a7ee25004c56d40aa9adfb
Rohit Nimkar <nehalnimkar@gmail.com>
2022-09-04 21:25:55 +0530

    📸 add screenshots

--- Makefile
+++ Makefile
@@ -1,13 +1,22 @@
-source:
-	cmake -Brohit
+BUILD_DIR := rohit
+OPENOCD_HOME := /usr/local/bin/openocd
+CPU_CORES :=1
+CFG_FILE :=stm32f1x.cfg
 
-build: source
-	${MAKE} -C rohit -j11
+
+all: source
+	${MAKE} -C $(BUILD_DIR) -j$(CPU_CORES)
+
+source:
+	cmake -B$(BUILD_DIR)
+
+build:
+	${MAKE} -C $(BUILD_DIR) -j$(CPU_CORES)
 
 flash:
-	openocd -d2 -s /usr/local/bin/openocd/scripts -f interface/stlink.cfg -c "transport select hla_swd" -f target/stm32f1x.cfg -c "program {./rohit/zephyr/zephyr.elf}  verify reset; shutdown;"
+	openocd -d2 -s $(OPENOCD_HOME)/scripts -f interface/stlink.cfg -c "transport select hla_swd" -f target/$(CFG_FILE) -c "program {./$(BUILD_DIR)/zephyr/zephyr.elf}  verify reset; shutdown;"
 
 clean:
-	rm -rf rohit
+	rm -rf $(BUILD_DIR)
 
 .phony: flash clean
--- src/main.c
+++ src/main.c
@@ -2,14 +2,70 @@
 #include <zephyr/zephyr.h>
 #include <zephyr/sys/printk.h>
 
+#define MY_STACK_SIZE 512
+
+static void thread1_handler(void *, void *, void *);
+static void thread2_handler(void *, void *, void *);
+
+K_THREAD_STACK_DEFINE(stack1, MY_STACK_SIZE);
+struct k_thread thread1_data;
+
+K_THREAD_STACK_DEFINE(stack2, MY_STACK_SIZE);
+struct k_thread thread2_data;
+
 void main(void)
 {
+    k_thread_create(&thread1_data, stack1,
+                    K_THREAD_STACK_SIZEOF(stack1),
+                    thread1_handler,
+                    NULL, NULL, NULL,
+                    4, 0, K_NO_WAIT);
+
+    k_thread_create(&thread2_data, stack2,
+                    K_THREAD_STACK_SIZEOF(stack2),
+                    thread2_handler,
+                    NULL, NULL, NULL,
+                    2, 0, K_NO_WAIT);
+}
+
+/**
+ * @brief task_handler function for thread 1
+ *  - after spawning, thread1 begins executing this function
+ * 
+ * @param unused1 1st parameter
+ * @param unused2 2nd parameter
+ * @param unused3 3rd parameter
+ */
+static void thread1_handler(void *unused1, void *unused2, void *unused3)
+{
+    ARG_UNUSED(unused1);
+    ARG_UNUSED(unused2);
+    ARG_UNUSED(unused3);
 
     while (1)
     {
-        printk("Hello World\n");
+        printk("Thread 1\n");
         k_msleep(1000);
     }
-    
+}
 
+/**
+ * @brief task_handler function for thread 2
+ *  - after spawning, thread2 begins executing this function
+ * 
+ * @param unused1 1st parameter
+ * @param unused2 2nd parameter
+ * @param unused3 3rd parameter
+ */
+static void thread2_handler(void *unused1, void *unused2, void *unused3)
+{
+    ARG_UNUSED(unused1);
+    ARG_UNUSED(unused2);
+    ARG_UNUSED(unused3);
+
+    while (1)
+    {
+        printk("Thread 2\n");
+        k_msleep(350);
+    }
 }
